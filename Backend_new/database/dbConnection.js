d1242b52a249d2acbe3e2cf87c23a5a54db014fa
chahatrajput5585 <106544104+chahatrajput5585@users.noreply.github.com>
2024-04-11 15:05:05 +0530

    final

--- Backend_new/database/dbConnection.js
+++ Backend_new/database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = () => {
   mongoose
-    .connect(process.env.MONGO_URI, {
+    .connect(process.env.MONGO_URL, {
       dbName: "MERN_JOB_SEEKING_WEBAPP",
     })
     .then(() => {
