66a1f51e8e826b94e2d4cde9da35d1f414083f45
chahatrajput5585 <106544104+chahatrajput5585@users.noreply.github.com>
2024-04-15 16:01:20 +0530

    Update auth.js

--- Backend_new/middlewares/auth.js
+++ Backend_new/middlewares/auth.js
@@ -4,16 +4,25 @@ import ErrorHandler from "./error.js";
 import jwt from "jsonwebtoken";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-  const { token } = req.cookies;
+  const token = req.cookies.token; // Extract token from cookies
   if (!token) {
     return next(new ErrorHandler("User Not Authorized", 401));
   }
-  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-  req.user = await User.findById(decoded.id);
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    const user = await User.findById(decoded.id);
 
-  next();
+    if (!user) {
+      return next(new ErrorHandler("User not found", 404));
+    }
+
+    // Attach user to the request object
+    req.user = user;
+    next();
+  } catch (error) {
+    return next(new ErrorHandler("Invalid Token", 401));
+  }
 });
-
 
 
